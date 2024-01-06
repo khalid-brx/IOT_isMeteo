c4c794a4e2d8f5078f1b6ee688b600cc1705b4af
Khalid Buarramou <151578799+khalid-brx@users.noreply.github.com>
2024-01-06 14:52:31 +0100

    V1.0

---
+++ IOT_project/DHT_app/parameters.py
@@ -0,0 +1,24 @@
+targetBotsDic = {
+    0: '6772123987:AAFJ9qJYnfeFO2cAKvzoFUkVPXqpsqM_PXE',
+    1: '6711503297:AAGu661FUbKPRkbei3IXr54jwQ_TOlrwv-o',
+    2: '6635208517:AAFTG_n0R9t4pfWE86fnAccFWrZ_vj72s7s',
+    3: '6669250305:AAHmzL8V8plzqSRsjYOB1HwwjIMdVRysAd4'
+}
+
+targetEmailsDic = {
+    0: 'khalid.buarramou20@ump.ac.ma',
+    1: 'khalidbouarramo@gmail.com',
+    2: 'jackolika@gmail.com',
+    3: 'jackolika@gmail.com'
+}
+
+targetNumbersDic = {
+    0: "212624083045",
+    1: "212624083045",
+    2: "212624083045",
+    3: "212624083045"
+}
+
+temperatureThreshold = 27
+humidityThreshold = 65
+
